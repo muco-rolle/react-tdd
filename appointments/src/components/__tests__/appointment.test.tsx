6cb866ff3d4b788724721de2c540dc83342ec622
Muco <mucotresor@gmail.com>
2023-03-16 12:24:35 +0200

    refactor: create a render utils function

--- appointments/src/components/__tests__/appointment.test.tsx
+++ appointments/src/components/__tests__/appointment.test.tsx
@@ -1,25 +1,29 @@
+import { ReactNode } from "react";
 import { createRoot } from "react-dom/client";
 import { act } from "react-dom/test-utils";
 import { Appointment } from "../appointment";
 
 describe("Appointment", () => {
+  let container: HTMLDivElement;
+
+  const render = (component: ReactNode) => {
+    return act(() => createRoot(container).render(component));
+  };
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.replaceChildren(container);
+  });
+
   it("renders the customer's first name", () => {
     const customer = { firstName: "Ashley" };
-    const container = document.createElement("div");
-    const component = <Appointment customer={customer} />;
-
-    document.body.replaceChildren(container);
-    act(() => createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Ashley");
   });
 
   it("renders another customer's first name", () => {
     const customer = { firstName: "Jordan" };
-    const container = document.createElement("div");
-    const component = <Appointment customer={customer} />;
-
-    document.body.replaceChildren(container);
-    act(() => createRoot(container).render(component));
+    render(<Appointment customer={customer} />);
     expect(document.body.textContent).toContain("Jordan");
   });
 });
