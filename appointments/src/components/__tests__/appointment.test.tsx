828651a1c4cc63541681f5d892478c8b786220a9
Muco <mucotresor@gmail.com>
2023-03-16 11:57:11 +0200

    refactor: use replaceChildren instead of replaceChild

--- appointments/src/components/__tests__/appointment.test.tsx
+++ appointments/src/components/__tests__/appointment.test.tsx
@@ -8,10 +8,18 @@ describe("Appointment", () => {
     const container = document.createElement("div");
     const component = <Appointment customer={customer} />;
 
-    document.body.appendChild(container);
-
+    document.body.replaceChildren(container);
     act(() => createRoot(container).render(component));
-
     expect(document.body.textContent).toContain("Ashley");
+  });
+
+  it.skip("renders another customer's first name", () => {
+    const customer = { firstName: "Jordan" };
+    const container = document.createElement("div");
+    const component = <Appointment customer={customer} />;
+
+    document.body.appendChild(container);
+    act(() => createRoot(container).render(component));
+    expect(document.body.textContent).toContain("Jordan");
   });
 });
