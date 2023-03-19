1241c14302343584d1ca831a5d021b409522d407
Muco <mucotresor@gmail.com>
2023-03-19 12:36:12 +0200

    refactor: remove duplication code

--- appointments/src/components/__tests__/appointment.test.tsx
+++ appointments/src/components/__tests__/appointment.test.tsx
@@ -31,6 +31,12 @@ describe("Appointment", () => {
 describe("AppointmentDayView", () => {
   let container: HTMLDivElement;
 
+  const today = new Date();
+  const appointments = [
+    { startsAt: today.setHours(12, 0) },
+    { startsAt: today.setHours(13, 0) },
+  ];
+
   const render = (component: ReactNode) => {
     return act(() => createRoot(container).render(component));
   };
@@ -52,24 +58,12 @@ describe("AppointmentDayView", () => {
   });
 
   it("renders a li element to display each appointment time", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) },
-    ];
-
     render(<AppointmentsDayView appointments={appointments} />);
     const elements = document.querySelectorAll("li");
     expect(elements).toHaveLength(2);
   });
 
   it("renders the time of each appointment", () => {
-    const today = new Date();
-    const appointments = [
-      { startsAt: today.setHours(12, 0) },
-      { startsAt: today.setHours(13, 0) },
-    ];
-
     render(<AppointmentsDayView appointments={appointments} />);
 
     const elements = document.querySelectorAll("li");
