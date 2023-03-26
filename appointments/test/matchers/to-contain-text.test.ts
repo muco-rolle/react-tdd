85bc1ea975c127229ab88520fbd3a205230b3469
Muco <mucotresor@gmail.com>
2023-03-26 10:59:15 +0200

    refactor: use simple data

--- appointments/test/matchers/to-contain-text.test.ts
+++ appointments/test/matchers/to-contain-text.test.ts
@@ -3,18 +3,17 @@ import { toContainText } from "./to-contain-text";
 
 describe("toContainText matcher", () => {
   it("returns pass true when the text given is found in the given DOM element", () => {
-    const element = document.createElement("div");
-    element.textContent = "text to find";
+    const element = { textContent: "text to find" };
 
     const result = toContainText(element, "text to find");
     expect(result.pass).toBe(true);
   });
 
   it("returns pass false when the text give is not found in the given DOM element", () => {
-    const element = document.createElement("div");
-    element.textContent = "";
+    const element = { textContent: "" };
 
     const result = toContainText(element, "text to find");
+
     expect(result.pass).toBe(false);
   });
 });
--- appointments/test/matchers/to-contain-text.ts
+++ appointments/test/matchers/to-contain-text.ts
@@ -1,3 +1,7 @@
-export function toContainText(received: Element, expectedText: string) {
+type Received = {
+  textContent: string;
+};
+
+export function toContainText(received: Received, expectedText: string) {
   return { pass: received.textContent?.includes(expectedText) };
 }
